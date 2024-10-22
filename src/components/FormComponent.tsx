29efae99b1600f023ed13d36b090e71c4b5a3661
jukitot <irzhyckaya@gmail.com>
2024-10-22 13:14:49 +0300

    hw4-done

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,22 +1,58 @@
 import React from 'react';
 import {useForm} from "react-hook-form";
+
 type FormProps = {
     username: string;
     password: string;
     age: number;
 }
 const FormComponent = () => {
-    const {handleSubmit, register} = useForm<FormProps>();
+    const {
+        handleSubmit,
+        register,
+        formState: {
+            errors,
+            isValid
+        }
+    } = useForm<FormProps>({mode: 'all'});
 
-    const customHandler= (dataFromForm: FormProps) =>{
+    const customHandler = (dataFromForm: FormProps) => {
         console.log(dataFromForm);
+        console.log(errors);
+        console.log(isValid);
     }
     return (
         <div>
-            <form onSubmit={handleSubmit(customHandler)} >
-                <input type="text" placeholder={'username'} {...register('username')}/>
-                <input type="text" placeholder={'password'} {...register('password')}/>
-                <input type="number" placeholder={'age'} {...register('age')}/>
+            <form onSubmit={handleSubmit(customHandler)}>
+                <label>
+                    <input type="text" placeholder={'username'} {...register('username', {
+                        required: {
+                            value: true,
+                            message: 'username is required'
+                        }
+                    })}/>
+                </label>
+                {
+                    errors.username && <div> {errors.username.message}</div>
+                }
+                <label>
+                    <input type="text" placeholder={'password'} {...register('password', {
+                        minLength: {value: 3, message: 'too short'},
+                        maxLength: {value: 6, message: 'too long'}
+                    })}/>
+                </label>
+                {
+                    errors.password && <div> {errors.password.message} </div>
+                }
+                <label>
+                    <input type="number" placeholder={'age'} {...register('age', {
+                        min: {value: 0, message: 'too young'},
+                        max: {value: 121, message: 'too old'}
+                    })}/>
+                </label>
+                {
+                    errors.age && <div> {errors.age.message} </div>
+                }
                 <button>Save</button>
             </form>
         </div>
