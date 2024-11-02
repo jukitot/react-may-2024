2e75894d9660084b4858532e6df76d441c19f9fe
jukitot <irzhyckaya@gmail.com>
2024-11-02 19:52:41 +0200

    lesson7-useState-useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import './App.css';
 
 const App:FC = () => {
+    let [number, setNumber] = useState<number>(0)
+
+    useEffect(() => {
+        console.log('connect to server, send to server', number);
+        return () => {
+            console.log('unconnect from server');
+        }
+    }, [number]);
   return (
       <>
+          <h2>{number}</h2>
+          <button onClick={()=>{
+              setNumber(prevState => prevState+1)
+          }}>increment</button>
+          <button onClick={()=>{
+              setNumber(prevState => prevState-1)
+          }}>decrement</button>
       </>
   );
 }
