1f8ac68d560905d0384f2e4d73d12314a1461a94
jukitot <irzhyckaya@gmail.com>
2024-11-06 16:11:33 +0200

    lesson8-zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React, {FC} from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+import {loadUser, loadUsers, removeUser, useAppDispatch, useAppSelector, userSlice} from "./redux/store";
 
-const App:FC = () => {
+function App() {
+   let userSliceState = useAppSelector(store=>store.userSliceHimself);
+
+   let dispatch = useAppDispatch();
+
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(users => {
+                dispatch(loadUsers(users));
+                // dispatch(removeUser(1))
+            })
+    }, []);
   return (
+
       <>
+          {
+              userSliceState.users.map((user)=>(<div>{user.username}</div>))
+          }
       </>
   );
 }
